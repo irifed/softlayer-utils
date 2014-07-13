6bfd163bd6198905f16c11349315347b40d74a84
IRINA FEDULOVA <i.fedulova@ru.ibm.com>
2014-07-14 00:59:17 +0400

    add nmon, git, python-pip

--- cci-init.sh
+++ cci-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y vim tmux nethogs
+apt-get install -y vim tmux htop nmon nethogs git python-pip
 
 wget -O /root/.vimrc https://raw.githubusercontent.com/irifed/dotfiles-pub/master/.vimrc
 wget -O /root/.tmux.conf https://raw.githubusercontent.com/irifed/dotfiles-pub/master/.tmux.conf
