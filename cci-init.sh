c58dc1faf47874079db0ace3c167838dc2842cd2
IRINA FEDULOVA <i.fedulova@ru.ibm.com>
2014-07-14 01:13:21 +0400

    add vnc4server, openbox, firefox

--- cci-init.sh
+++ cci-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y vim tmux htop nmon nethogs git python-pip
+apt-get install -y vim tmux htop nmon nethogs git python-pip vnc4server openbox firefox
 
 wget -O /root/.vimrc https://raw.githubusercontent.com/irifed/dotfiles-pub/master/.vimrc
 wget -O /root/.tmux.conf https://raw.githubusercontent.com/irifed/dotfiles-pub/master/.tmux.conf
